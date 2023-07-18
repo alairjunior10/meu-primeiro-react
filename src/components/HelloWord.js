bd6a167d831a822517eb1b447cb45e87f3a1e1c5
alairjunior10 <alairpinheirojunior@gmail.com>
2023-07-17 23:20:35 -0300

    Curso React aula 4 - Criando componentes no React

---
+++ src/components/HelloWord.js
@@ -0,0 +1,14 @@
+import Frase from './Frase';
+
+function HelloWord (){
+
+    return (
+        <div>
+            <h1>Meu Primeiro componente</h1>
+            <Frase/>
+            <Frase/>
+        </div>
+    )
+}
+
+export default HelloWord;
